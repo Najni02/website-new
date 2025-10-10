5c9224a115b0031f6f54d7db5682ddf934320a5c
Najni02 <jan.amrhein@bluewin.ch>
2025-10-11 00:58:41 +0200

    swiper setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  cssMode: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
