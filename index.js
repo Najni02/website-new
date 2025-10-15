9845916b5b391f25cd7df7498c0adb281d1b60e2
Najni02 <jan.amrhein@bluewin.ch>
2025-10-15 21:04:39 +0200

    index contact, all sites: basic setup

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
 
-  cssMode: true,
-  // Navigation arrows
+  const swiper = new Swiper(".swiper", {
+    centeredSlides: true,
+    slidesPerView: 1.5,  // Slides passen sich an
+    spaceBetween: 500,
+    grabCursor: true,
+    loop: true,
+    effect: "coverflow",
+
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 500,
+      modifier: 1,
+      slideShadows: false,
+    },
+  //  breakpoints: {
+  //    320: { slidesPerView: 1.1 },
+  //    640: { slidesPerView: 1.5 },
+  //    1024: { slidesPerView: 2.5 },
+  //  },
+
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+  });
 
-
-});
